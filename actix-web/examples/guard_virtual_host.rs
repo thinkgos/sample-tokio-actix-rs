193623a7cab2d9565c6da8a4562f467c9416ff6c
thinkgo <thinkgo@aliyun.com>
2022-11-19 16:04:31 +0800

    feat; add more actix example

---
+++ actix-web/examples/guard_virtual_host.rs
@@ -0,0 +1,22 @@
+use actix_web::{guard, web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::scope("/")
+                    .guard(guard::Header("Host", "www.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
+            )
+            .service(
+                web::scope("/")
+                    .guard(guard::Header("Host", "users.rust-lang.org"))
+                    .route("", web::to(|| async { HttpResponse::Ok().body("user") })),
+            )
+            .route("/", web::to(HttpResponse::Ok))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
